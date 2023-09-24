96128ccb7167184f7731581fc89c60d534a36762
CastilloValeria <casvaleli87@gmail.com>
2023-09-24 15:30:11 -0300

    app.js habia quedado fuera de la carpeta, reincertando

---
+++ app.js
@@ -0,0 +1,34 @@
+const {leerArchivo}= require ("./modulos/archivos.js");
+const { argv } = require("process");
+
+let archivo= leerArchivo;
+
+
+
+function calculadora (a, b, callback) {
+   callback =operacion;
+    switch (operacion) {
+        case "suma":
+         const sumar= require ("./modulos/suma.js");
+            break;
+        case "resta":
+         const restar= require ("./modulos/resta.js");  
+            break;
+        case "multiplicacion":
+         const multiplicacion= require("./modulos/multiplicacion.js");
+            break; 
+        case "dividir":
+         const dividir= require("./modulos/division.js");
+            break;                   
+        default:
+         "ingrese una operacion de las siguientes opciones: suma/ resta/ multiplicar/dividir."
+            break;
+    }
+    return callback;
+}
+    let a =process.argv[2];
+    let b =process.argv[3];
+    let operacion =process.argv[4];
+//revisar
+
+console.log(calculadora())
